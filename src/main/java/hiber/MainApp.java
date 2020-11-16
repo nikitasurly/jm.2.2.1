93378b222624f12a62b6f083e94e5eab3e4a3efd
nikita <mikropyl28@gmail.com>
2020-11-16 21:45:24 +0300

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,55 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        User user1 = new User(
+                "User1", "Lastname1", "user1@mail.ru");
+        Car car1 = new Car("model1", 1);
+        user1.setUserCar(car1);
+        car1.setUser(user1);
+        userService.add(user1, car1);
+
+        User user2 = new User(
+                "User2", "Lastname2", "user2@mail.ru");
+        Car car2 = new Car("model2", 2);
+        user2.setUserCar(car2);
+        car2.setUser(user2);
+        userService.add(user2, car2);
+
+        User user3 = new User(
+                "User3", "Lastname3", "user3@mail.ru");
+        Car car3 = new Car("model3", 3);
+        user3.setUserCar(car3);
+        car3.setUser(user3);
+        userService.add(user3, car3);
+
+        User user4 = new User(
+                "User4", "Lastname4", "user4@mail.ru");
+        Car car4 = new Car("model4", 4);
+        user4.setUserCar(car4);
+        car4.setUser(user4);
+        userService.add(user4, car4);
+
+
+        List<User> users = userService.listUsers();
+        users.forEach(System.out::println);
+
+        System.out.println(userService.getUserByCar("model2", 2));
+
+        context.close();
+   }
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,61 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="cars")
+public class Car {
+
+    @OneToOne(mappedBy = "userCar", cascade = CascadeType.ALL)
+    private User user;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String model;
+
+    @Column
+    private int series;
+
+    public Car(){}
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
